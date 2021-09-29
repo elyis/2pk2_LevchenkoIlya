3b657f7faf5bb56cd3f56ab376fdab11e423ab4a
Илья Левченко <elyi7367@gmail.com>
2021-09-29 11:42:25 +0500

    d

---
+++ pz_6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y;
+            for(double x = -2; x <= 2 ;x += 0.5)
+            {
+                y = -2.4 * x * x + 5 * x - 3;
+                Console.WriteLine("x = " + x + " y = " + y);
+            }
+            
+        }
+    }
+}

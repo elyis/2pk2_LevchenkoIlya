a7f326fb8b4a6b271b0744a89e481b0eb5721545
Илья Левченко <elyi7367@gmail.com>
2021-10-14 10:41:26 +0500

    d

---
+++ pz_7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace pz_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+            Random rnd = new Random();
+            for(int i = 0;i < nums.Length;i++)
+                nums[i] = rnd.Next(-11, 11);
+            for (int i = 0; i < nums.Length; i++)
+                Console.WriteLine(nums[i]);
+
+            for(int i = 0,j = nums.Length - 1;i < nums.Length /2 ;i++,j--)
+            {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+
+            Console.WriteLine("Reverse:");
+            for (int i = 0; i < nums.Length; i++)
+                Console.WriteLine(nums[i]);
+        }
+
+    }
+}

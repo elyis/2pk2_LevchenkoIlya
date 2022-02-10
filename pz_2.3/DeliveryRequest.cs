c3bf5555a8f116d50c1eb60728db804cdf0cd8ee
Илья Левченко <elyi7367@gmail.com>
2022-02-10 07:36:57 +0500

    nntv

---
+++ pz_2.3/DeliveryRequest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pz_2._3
+{
+    using System;
+
+    public class DeliveryRequest
+    {
+            private int id;
+            static private int[] ids = new int[400];
+            static private int numOfDelivery = 0;
+
+            private string requestTime;
+            private string adr;
+            private float summ;
+
+
+            public DeliveryRequest(string address = "В ближайшем ресторане")
+            {
+                adr = address;
+                DeliveryRegistration();
+            }
+
+            //Этот конструктор бесполезен,т.к предыдущий выполняет его задачи, а нужно было несколько
+            public DeliveryRequest()
+            {
+                adr = "В ближайшем ресторане города";
+                DeliveryRegistration();
+            }
+
+            public void GetDeliveryInfo()
+            {
+                Console.WriteLine("Заказ по адресу: " + adr +
+                    " произошел в " + requestTime +
+                    "\nИтоговая сумма заказа составляет " + summ);
+            }
+
+            //Установка времени заказа, вычисление стоимости и проверка уникальности id заказа
+            private void DeliveryRegistration()
+            {
+                requestTime = DateTime.Now.ToString();
+                summ = OrderCalculation();
+
+                Random rnd = new Random();
+                int temp = rnd.Next(0, (int)Math.Pow(10, 8));
+                for (int i = 0; i < numOfDelivery; i++)
+                {
+                    if (ids[i] == temp)
+                        temp = rnd.Next(0, (int)Math.Pow(10, 8));
+                }
+
+                id = temp;
+                ids[numOfDelivery] = temp;
+                numOfDelivery++;
+            }
+
+            private int OrderCalculation()
+            {
+                Random rnd = new Random();
+                return rnd.Next(100, 10000);
+            }
+
+    }
+
+}
---
+++ pz_2.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using pz_2._3;
+
+//Покупатель может указать только адрес доставки, т.к id, сумма и время регистрации должны фиксироваться системой 
+
+//14 вариант
+namespace pz_2._3
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            DeliveryRequest a = new DeliveryRequest();
+            a.GetDeliveryInfo();
+            DeliveryRequest a2 = new DeliveryRequest("Гамбург");
+            a2.GetDeliveryInfo();
+            DeliveryRequest a3 = new DeliveryRequest("Этот код не выполняет множество проверок");
+            a3.GetDeliveryInfo();
+            DeliveryRequest a4 = new DeliveryRequest("ПОэтому можно заниматься этим");
+            a4.GetDeliveryInfo();
+        }
+    }
+}
+//P.s: код можно было упростить разделив данный класс на заказ и множество заказов

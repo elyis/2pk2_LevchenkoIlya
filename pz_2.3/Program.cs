60452888c60e45c3fe3edcf5fb474a646f8c74a1
Илья Левченко <elyi7367@gmail.com>
2022-02-22 13:29:55 +0500

    Revert "Delete pz_2.3 directory"
    This reverts commit d818b0e5d9c155a321cdd8de51ea7c06f11cc970.

---
+++ pz_2.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using pz_2._3;
+
+//Покупатель может указать только адрес доставки, т.к id, сумма и время регистрации должны фиксироваться системой 
+
+//14 вариант
+namespace pz_2._3
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            RejectedDelivery a = new RejectedDelivery(null);
+            a.GetDeliveryInfo();
+            RejectedDelivery a2 = new RejectedDelivery("Гамбург");
+            a2.GetDeliveryInfo();
+            RejectedDelivery a3 = new RejectedDelivery("Этот код не выполняет множество проверок");
+            a3.GetDeliveryInfo();
+            RejectedDelivery a4 = new RejectedDelivery("ПОэтому можно заниматься этим");
+            a4.GetDeliveryInfo();
+        }
+    }
+}
+//P.s: код можно было упростить разделив данный класс на заказ и множество заказов

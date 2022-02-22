eaf95c8be3085ce8076a5ba614f8204abd1d7086
Илья Левченко <elyi7367@gmail.com>
2022-02-22 13:36:54 +0500

    ...

--- pz_2.3/DeliveryRequest.cs
+++ pz_2.3/DeliveryRequest.cs
@@ -82,37 +82,6 @@ namespace pz_2._3
             return rnd.Next(100, 10000);
         } 
 
-        enum statusErrorOfDelivery
-        {
-            ALLRIGHT = 0,
-            ADDRESS_ERROR
-        }
-
-    }
-
-    class RejectedDelivery : DeliveryRequest
-    {
-        public RejectedDelivery(string address): base(address) 
-        { 
-           if (base.isTypeError == 1)
-                Reason = "Ошибка в введенном адресе";
-        }
-
-        private string reason;
-        public string Reason
-        {
-            set => reason = value;
-            get => reason;
-        }
-        
-        public override void GetDeliveryInfo()
-        {
-            if (base.isTypeError == 1)
-                Console.WriteLine("Статус заказа не оформлен по причине: " + Reason);
-            else
-                base.GetDeliveryInfo();
-        }
-
     }
 
 }
---
+++ pz_2.3/RejectedDelivery.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pz_2._3
+{
+
+    enum statusErrorOfDelivery
+    {
+        ALLRIGHT = 0,
+        ADDRESS_ERROR
+    }
+
+    class RejectedDelivery : DeliveryRequest
+    {
+        public RejectedDelivery(string address) : base(address)
+        {
+            if (base.isTypeError == 1)
+                Reason = "Ошибка в введенном адресе";
+        }
+
+        private string reason;
+        public string Reason
+        {
+            set => reason = value;
+            get => reason;
+        }
+
+        public override void GetDeliveryInfo()
+        {
+            if (base.isTypeError == 1)
+                Console.WriteLine("Статус заказа не оформлен по причине: " + Reason);
+            else
+                base.GetDeliveryInfo();
+        }
+
+    }
+}
